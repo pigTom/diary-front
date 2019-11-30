d634e48df2bee93da5f7e69715ef605995dfa10a
pigtom <13393895196@163.com>
2019-11-30 18:26:50 +0800

    商品改成80, api ip: 172.17.0.2:8088

--- src/app/service/http-config.service.ts
+++ src/app/service/http-config.service.ts
@@ -7,6 +7,6 @@ export class HttpConfigService {
 
   constructor() { }
 
-  public ip = 'http://localhost:8089';
+  public ip = 'http://172.17.0.2:8088';
   public BASE_API = this.ip + '/api/v0.1';
 }
