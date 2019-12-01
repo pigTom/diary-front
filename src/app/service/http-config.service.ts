6851141d8927a32670b18397f448d61aa91e267c
pigtom <13393895196@163.com>
2019-12-01 16:24:00 +0800

    暂时用jenkins的ip部署nodejs项目，然后npm start 中指定主机 0.0.0.0而不是localhost

--- src/app/service/http-config.service.ts
+++ src/app/service/http-config.service.ts
@@ -7,6 +7,6 @@ export class HttpConfigService {
 
   constructor() { }
 
-  public ip = 'http://172.17.0.2:8088';
+  public ip = 'http://172.17.0.2:8888';
   public BASE_API = this.ip + '/api/v0.1';
 }
